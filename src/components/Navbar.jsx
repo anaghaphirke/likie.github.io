1aa72a6aff4ae4cbb4860cca0d4e42c2788f835a
anaghaphirke <90553930+anaghaphirke@users.noreply.github.com>
2021-09-25 10:58:45 +0530

    1st draft updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,52 +1,41 @@
-import React, {Component}from 'react';
-import {BrowserRouter as Router,Link,NavLink,Switch,Route,withRouter} from "react-router-dom";
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-class Navbar extends Component{
-    constructor(props){
-        super(props);
-        this.handleProfileButton = this.handleProfileButton.bind(this); 
-        this.handleEditButton = this.handleEditButton.bind(this); 
-        this.handleLogoutButton = this.handleLogoutButton.bind(this); 
-    }
-    
-    handleProfileButton(){
-            this.props.history.push("/profile");
-    }
 
-    handleEditButton(){
-        this.props.history.push("/edit");
+class Navbar extends React.Component{
+    render(){
+      return(
+              <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+                <a class="navbar-brand">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                  <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                    <li class="nav-item home" >
+                      <NavLink to="/" class="nav-link">Home</NavLink>
+                    </li>
+                    <li class="nav-item contact">
+                      <NavLink class="nav-link" activeClassName="active" to = "/contact">Contact</NavLink>
+                    </li>
+                    <li class="nav-item about" >
+                      <NavLink to = "/about"  class="nav-link">About</NavLink>
+                    </li>
+                    <li class="nav-item signin" >
+                      <NavLink to = "/signin" class="nav-link"><i class="fas fa-user"></i>Sign In</NavLink>
+                    </li>
+                    <li class="nav-item login" >
+                      <NavLink to = "/login" class="nav-link"><i class="fas fa-sign-in-alt"></i>Log In</NavLink>
+                    </li>
+                    <li class="nav-item cart">
+                      <NavLink to = "/cart" class="nav-link"><i class="fas fa-shopping-cart"></i></NavLink>
+                    </li>
+                  </ul>          
+                </div>
+              </nav>
+
+      );
     }
-    handleLogoutButton(){
-        this.props.history.push("/");
-    }
-    render()
-    {
-        return(
-            <>
-                <Router>
-                    <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-primary" >
-                        <div onClick={this.handleProfileButton} className="navbar-brand"><img src ="/images/profile.jpg" className="img-fluid rounded-circle" 
-                            height="50" width="50" />
-                        </div>
-                        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        
-                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-                                <li className="nav-item" >
-                                    <div onClick={this.handleEditButton} className="nav-link navMenu">
-                                        <i className="fas fa-user-edit"></i></div>
-                                </li>
-                                <li className="nav-item">
-                                    <div onClick={this.handleLogoutButton} className="nav-link navMenu">
-                                        <i className="fas fa-sign-out-alt"></i></div>
-                                </li>
-                            </ul>          
-                        </div>
-                    </nav>
-                </Router>
-            </>
-        )} 
-}
-export default withRouter(Navbar)
+  }
+  export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,43 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
-import Login from './components/Login'
-import Wall from './components/Wall'
-import Signup from './components/Signup'
-import Profile from './components/Profile'
+import reportWebVitals from './reportWebVitals';
+import Navbar from './components/Navbar';
+import { BrowserRouter,Switch, Route } from 'react-router-dom'; 
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About';
+import Signin from './components/Signin';
+import Login from './components/Login';
+import Cart from './components/Cart';
 
-const userData={
-  name:"Nikhil Phirke",
-  userid:"@nikphirke",
-  email:"nik@gmail.com",
-  city:"Akola",
-  state:"Maharashtra"
-}
-
+const NavPage = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/signin">
+        <Signin />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/cart">
+        <Cart />
+      </Route>
+    </Switch>
+  );
+};
 
 ReactDOM.render(
-  <>
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Login} />
-      <Route path="/wall" component={Wall}/>
-      <Route path="/signup" component={Signup}/>
-      <Route path="/profile">
-          <Profile profileDisabled={true} buttonText="Back" pageName = "Profile" userData={userData}/>
-      </Route>
-      <Route path="/edit">
-          <Profile  buttonText="Save" pageName= "Edit" userData={userData}/>
-      </Route>
-
-    </Switch>
-  </Router>
-</>
-  ,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Navbar /> 
+    <NavPage />
+  </BrowserRouter>,
+  document.getElementById('app')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-export default userData
+reportWebVitals();
+
+
+
+//import Navbar from './components/navbar'
+
